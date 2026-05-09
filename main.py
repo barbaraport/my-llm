2de37ecea801625968321be593b1ffa3f00f3f59
barbaraport <62317075+barbaraport@users.noreply.github.com>
2026-05-09 20:28:25 -0300

    feature(openai): requests a fun fact to llama3.2 with openai lib

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from openai import OpenAI
 
 from settings import Settings
 
@@ -8,5 +9,7 @@ settings = Settings()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
-
+    ai = OpenAI(base_url=settings.API_BASE_URL, api_key=settings.API_KEY.get_secret_value())
+    request = ai.chat.completions.create(model="llama3.2", messages=[{"role": "user", "content": "Tell me a fun fact"}])
+    response = request.choices[0].message.content
+    return {"response": response}
