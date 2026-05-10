fb6a9f6ad0bf3366cb5644f54ca1169c753e888b
barbaraport <62317075+barbaraport@users.noreply.github.com>
2026-05-09 21:06:18 -0300

    refactor(model): choosing model through environment variables and validating this

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ settings = Settings()
 @app.get("/")
 def read_root():
     ai = OpenAI(base_url=settings.API_BASE_URL, api_key=settings.API_KEY.get_secret_value())
-    request = ai.chat.completions.create(model="llama3.2", messages=[{"role": "user", "content": "Tell me a fun fact"}])
+    request = ai.chat.completions.create(model=settings.MODEL_NAME, messages=[{"role": "user", "content": "Tell me a fun fact"}])
     response = request.choices[0].message.content
-    return {"response": response}
+    return {"answer": response, "model": settings.MODEL_NAME.value}
--- settings.py
+++ settings.py
@@ -1,9 +1,15 @@
+from enum import Enum
+
 from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+class AvailableModels(str, Enum):
+    LLAMA_3_2 = "llama-3.2"
+    DEEPSEEK_R1_1_5 = "deepseek-r1:1.5b"
 
 class Settings(BaseSettings):
     API_KEY: SecretStr = Field(...)
     API_BASE_URL: str = Field(...)
+    MODEL_NAME: AvailableModels = Field(...)
 
     model_config = SettingsConfigDict(env_file=".env")
